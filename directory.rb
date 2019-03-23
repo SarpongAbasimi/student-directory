1666a2b9b945626efb00157f6a3672ab7359ea51
sarpong <abasimisarpong@rocketmail.com>
2019-03-23 13:47:10 +0000

    A list of students have been added to the directory

---
+++ directory.rb
@@ -0,0 +1,17 @@
+puts 'The students of Directory Academy'
+puts '____________'
+puts 'Dr. Hannibal Lecter'
+puts 'Darth Vader'
+puts 'Nurse Ratched'
+puts 'Michael Corleone'
+puts 'Alex DeLarge'
+puts 'The wicked Witch of the West'
+puts 'Terminator'
+puts 'Freddu Krueger'
+puts 'The Joker'
+puts 'Joffrey Baratheon'
+puts 'Norman Bates'
+print 'Overall, we have '
+print 11
+puts ' great students'
+
