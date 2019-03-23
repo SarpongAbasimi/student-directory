71187cf8481ae6ec8d35b29495b60bb52818cfb0
sarpong <abasimisarpong@rocketmail.com>
2019-03-23 19:30:17 +0000

    Added and interactive menu

--- directory.rb
+++ directory.rb
@@ -20,8 +20,10 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student,index|
+    if student[:name].length < 12
+      puts "#{index + 1 }. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
@@ -29,7 +31,52 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  
+  students = [] 
+  name = gets.chomp 
+    
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  
+   students
+end
+
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students "
+    puts "2. show the students" 
+    puts "9. Exit"
+    
+    selection = gets.chomp 
+   
+    case selection
+    when "1"
+      students = input_students 
+    when "2" 
+      print_header
+      print(students)  
+      print_footer(students)
+    when "9"
+      exit
+    else 
+      puts "I don't know what you meant, try again"    
+    end   
+  end
+end
 #nothing happens until we call the methods
+
+interactive_menu()
+
+=begin
+students = input_students
 print_header
 print(students)
-print_footer(students) 
+print_footer(students)
+=end
