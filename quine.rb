d3f52a99b733a4e982660dd0a4c6d8ecdebdfb97
sarpong <abasimisarpong@rocketmail.com>
2019-03-26 16:55:46 +0000

    quine completed

--- quine.rb
+++ quine.rb
@@ -1,6 +1,5 @@
 # A quine is a program that prints out its own source code
 # This is my implementation of quine 
 
-
-name = "hello sarp"
-puts "name = " + name.inspect
+greet = "\nputs \"greet = \"Hello world\""
+puts "greet = " + greet.inspect + greet
